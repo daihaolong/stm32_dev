75f6141cbbefdc89391b4f1a197a65caff3ddb2d
daihaolong <351871231@qq.com>
2025-10-20 23:16:17 +0800

    key_led running ok

---
+++ Hardware/Key/key.c
@@ -0,0 +1,45 @@
+#include "key.h"
+
+void Key_Init()
+{
+    // 开启时钟
+    RCC->APB2ENR |= RCC_APB2ENR_IOPFEN;
+    RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
+    
+    // GPIOF10引脚输入模式mode00 下拉输入cnf10
+    GPIOF->CRH &= ~GPIO_CRH_MODE10;
+    GPIOF->CRH &= ~GPIO_CRH_CNF10_0;
+    GPIOF->CRH |= GPIO_CRH_CNF10_1;
+
+    GPIOF->ODR &= ~GPIO_ODR_ODR10;
+ 
+    // AFIO配置引脚复用选择器
+    AFIO->EXTICR[2] |= AFIO_EXTICR3_EXTI10_PF;
+
+    // EXTI配置
+
+    EXTI->RTSR |= EXTI_RTSR_TR10;
+    EXTI->IMR |= EXTI_IMR_MR10;
+
+    //配置nvic
+
+    NVIC_SetPriorityGrouping(3);   //全部都是抢占优先级
+    NVIC_SetPriority(EXTI15_10_IRQn, 3);
+    NVIC_EnableIRQ(EXTI15_10_IRQn);
+    
+}
+
+
+void EXTI15_10_IRQHandler(void ){
+
+    EXTI->PR |= EXTI_PR_PR10;
+    Delay_ms(10);
+    if ((GPIOF->IDR & GPIO_IDR_IDR10)!=0)
+    {
+        /* code */
+        LED_Toggle(LED1);
+    }
+    
+
+}
+
--- User/main.c
+++ User/main.c
@@ -1,27 +1,18 @@
-#include "delay.h"
 #include "led.h"
+#include "key.h"
 
 int main(void){
 
-uint16_t LEDS[3] = {LED1, LED2, LED3};
-// 开启PA的时钟
+
 LED_init();
-
+Key_Init();
 while (1){
-
-// 开启LED1  然后延迟500ms
-for ( uint8_t i = 0; i < 3; i++)
-{
-    /* code */
-    LED_On(LEDS[i]);
-    Delay_ms(500);
-    LED_Off(LEDS[i]);
-}
-
 
 };
 
 
 }
+
+
 
 
